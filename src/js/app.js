c6a3ad67a74b026ad51b7c71d761110174e40c86
Giuseppe Filiberto <83554043+GiuseppeFiliberto@users.noreply.github.com>
2021-07-22 05:49:55 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -13,13 +13,9 @@ function show() {
 }
 
 function close() {
-    mainMenu.style.top = '-100%';
-    closeMenu.style.top = '-100%';
+    mainMenu.style.top = '-130%';
+    closeMenu.style.top = '-130%';
 
     
 }
 
-// const toggleClose = document.getElementById('toggle');
-// toggle.onclick = function(){
-//     toggle.classlist.toggle('active');
-// }
